b8da3147f6366680ebbf6f157feeaef648176852
unknown <sinharoshan012@gmail.com>
2025-07-11 15:12:00 +0000

    updating assistant file test

--- assistant.py
+++ assistant.py
@@ -4,7 +4,6 @@ import openai
 # Make sure you set your API key before running:
 # export OPENAI_API_KEY="your_api_key"  (Linux/Mac)
 # setx OPENAI_API_KEY "your_api_key"    (Windows)
-#first testing
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
